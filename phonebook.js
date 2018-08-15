b96ff794d7286ffa44ef97cc1ac42d5c9d1f4064
Andrew <andrewchoi1@hotmail.com>
2018-08-15 11:36:22 -0400

    added json file refactored code

--- phonebook.js
+++ phonebook.js
@@ -1,7 +1,5 @@
 var readline = require('readline');
 var fs = require('fs');
-var http = require('http');
-var dns = require('dns');
 var promisify = require('util').promisify;
 
 var writeFile = promisify(fs.writeFile);
@@ -12,72 +10,100 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
+var drawMenu = function() {
+    console.log(
+        `Electronic Phone Book
+    =====================
+    1. Look up an entry
+    2. Set an entry
+    3. Delete an entry
+    4. List all entries
+    5. Quit`
+    );
+};
 
-var phonebook = [{ John: "444-4444-4444" }, { David: "111-1111-1111" }];
+var phonebook = [{ adam: '111-1111-1111' }, { david: '222-2222-2222' }];
 
-
-var appendToPhonebook = function() {
-    readFile('phonebook.txt').then(function(text) {
-        var phonebookText = JSON.stringify(text.toString())
-        phonebook.push(phonebookText)
-        console.log(phonebook)
-    });
-}
-
-appendToPhonebook()
-// console.log(phonebook)
-
-
-
-rl.question('What do you want to do (1-5)? ', function(num) {
-    if (Object.keys(menuOptions).includes(num)) {
-        menuOptions[num]();
-    } else {
-        console.log('invalid number');
-    }
-});
-
-var lookUpEntry = function() {
-    rl.question('What is the name?', function(name) {
-        for (var objects of phonebook) {
-            if(Object.keys(objects).includes(name)) {
-                console.log(objects[name])
-            }
+var main = function() {
+    drawMenu();
+    rl.question('What do you want to do (1-5)? ', function(menuOption) {
+        if (Object.keys(menuOptions).includes(menuOption)) {
+            menuOptions[menuOption](main);
+        } else {
+            rl.close();
         }
     });
 };
 
+var appendToPhonebook = function() {
+    readFile('phonebook.json').then(function(text) {
+        var phonebookText = JSON.parse(text.toString());
+        phonebook.push(phonebookText);
+    });
+};
 
+appendToPhonebook();
 
-var setEntry = function() {
+var appendtoFile = function() {
+    phonebook.forEach(function(phoneNum) {
+        var phoneNumJSON = JSON.stringify(phoneNum);
+        writeFile('phonebook.json', phoneNumJSON).then(function() {});
+    });
+};
+
+appendtoFile();
+
+var lookUpEntry = function(callback) {
+    rl.question('What is the name?', function(name) {
+        console.log(name);
+        for (var objects of phonebook) {
+            console.log(objects);
+            if (Object.keys(objects).includes(name)) {
+                console.log(objects[name]);
+            }
+        }
+        callback();
+    });
+};
+
+var setEntry = function(callback) {
     rl.question('What is the name? ', function(name) {
         rl.question('What is the phone number? ', function(phoneNum) {
-            var content = JSON.stringify({name: phoneNum})
             rl.close();
-            writeFile('phonebook.txt', content).then(function() {
-                console.log('Entry stored for' + name)
-            })
+            var content = {};
+            content[name] = phoneNum;
+            phonebook.push(content);
+            console.log(`entry stored for ${name}`);
+            callback();
         });
     });
 };
 
-var deleteEntry = function() {
+var deleteEntry = function(callback) {
     rl.question('What name do you want to delete? ', function(name) {
         if (Object.keys(phonebook).includes(name)) {
-            delete phonebook[name]
-            console.log(phonebook)
+            delete phonebook[name];
+            console.log(phonebook);
         }
-    })
-}
+        callback();
+    });
+};
 
-var listAllEntries = function() {
-    console.log(phonebook)
-}
+var listAllEntries = function(callback) {
+    console.log(phonebook);
+    callback();
+};
+
+var quit = function() {
+    rl.close();
+};
 
 var menuOptions = {
     1: lookUpEntry,
     2: setEntry,
     3: deleteEntry,
     4: listAllEntries,
-    5: 'Quit'
+    5: quit
 };
+
+main();
